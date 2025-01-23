145433fd23834c9186e14c4c956796a2d1dd4d18
Hyunjung <hyunjung.kim@g2e.co.kr>
2025-01-23 13:30:37 +0900

    추가: :tada: Spring 개발환경 세팅

---
+++ src/test/java/com/diaconn_mall/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diaconn_mall.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
