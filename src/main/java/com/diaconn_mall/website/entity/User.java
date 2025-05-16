3800a302568fda521ee234d60f9a0de38cd536ec
52sen <sein.jung@g2e.co.kr>
2025-05-16 17:00:48 +0900

    수정: ⚡ 모튼 user테이블의 모든 컬럼 추가

--- src/main/java/com/diaconn_mall/website/entity/User.java
+++ src/main/java/com/diaconn_mall/website/entity/User.java
@@ -5,22 +5,40 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
-@Table(name = "users")
+///Todo. user는 예약어로 테이블명으로 쓰면 오류가 나는 경우가 있다고함 (테이블 자체를 'users로' 변경해야될지 고민)
+@Table(name = "user")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+    private String phone;
+    private String password;
+    private String address;
 
     @Column(unique = true)
     private String email;
 
-    private String phone;
-    private String password;
-    private String address;
+    @Column(name = "address_detail")
+    private String addressDetail;
+
+    @Column(nullable = false)
+    private String signout = "N";
+
+    @Column(name = "join_date", nullable = false)
+    private LocalDateTime joinDate = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "outdate_at")
+    private LocalDateTime outdateAt;
 }
