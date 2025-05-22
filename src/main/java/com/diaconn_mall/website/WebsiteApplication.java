9d128a71dc6656d03fdc550ecae37c3d4a70f63e
hjkim1137 <hyunjung.kim@g2e.co.kr>
2025-05-23 00:21:51 +0900

    수정: :zap: userController 스캔 안되는 문제 해결을 위해 테스트 목적 @Bean 코드 추가

--- src/main/java/com/diaconn_mall/website/WebsiteApplication.java
+++ src/main/java/com/diaconn_mall/website/WebsiteApplication.java
@@ -2,12 +2,31 @@ package com.diaconn_mall.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.diaconn_mall.website")
 public class WebsiteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner checkBeans(ApplicationContext ctx) {
+		return args -> {
+			System.out.println(">>> 등록된 빈 목록 (controller 포함 여부 확인):");
+			for (String beanName : ctx.getBeanDefinitionNames()) {
+				if (beanName.contains("controller")) {
+					System.out.println(beanName);
+				}
+			}
+		};
+	}
 }
+
+
+
