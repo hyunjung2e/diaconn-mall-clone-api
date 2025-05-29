f558f24b16e984ce0c52dcd62959eefb0e7f386d
hjkim1137 <hyunjung.kim@g2e.co.kr>
2025-05-29 12:47:19 +0900

    수정: :zap: user id Long 타입으로 변경

--- src/main/java/com/diaconn_mall/website/dto/UserDto.java
+++ src/main/java/com/diaconn_mall/website/dto/UserDto.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
-    private int id;
+    private long id;
     private String name;
     private String email;
     private String phone;
