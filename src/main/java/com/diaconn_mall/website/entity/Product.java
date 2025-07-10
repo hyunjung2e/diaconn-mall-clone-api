dbc22b99991538a4ed8e4e6669a24a15ea2479c5
Hyunjung <hyunjung.kim@g2e.co.kr>
2025-07-10 13:23:41 +0900

    수정: :zap: img_url 매핑 오류 수정

--- src/main/java/com/diaconn_mall/website/entity/Product.java
+++ src/main/java/com/diaconn_mall/website/entity/Product.java
@@ -21,7 +21,7 @@ public class Product {
     private String desc;
     private Integer count;
     private Integer price;
-    @Column(name = "img_url")
+    @Column(name = "img_url", columnDefinition = "LONGTEXT")
     private String imgUrl;
     @Column(name = "alt_text")
     private String altText;
