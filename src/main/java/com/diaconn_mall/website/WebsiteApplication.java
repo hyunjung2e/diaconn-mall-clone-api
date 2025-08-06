5ed66f25ea1f5624a3c9f9b086bc24d7ed5823f7
hjkim1137 <hyunjung.kim@g2e.co.kr>
2025-08-06 22:58:54 +0900

    수정: :zap: 타임존을 Asia/Seoul로 지정

--- src/main/java/com/diaconn_mall/website/WebsiteApplication.java
+++ src/main/java/com/diaconn_mall/website/WebsiteApplication.java
@@ -1,11 +1,14 @@
 package com.diaconn_mall.website;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication
@@ -15,10 +18,16 @@ public class WebsiteApplication {
 		SpringApplication.run(WebsiteApplication.class, args);
 	}
 
+	// 타임존을 KST로 지정
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	@Bean
 	CommandLineRunner checkBeans(ApplicationContext ctx) {
 		return args -> {
-			System.out.println(">>> 등록된 빈 목록 (controller 포함 여부 확인):");
+			System.out.println("@@@@@@@ Spring 서버를 시작합니다");
 			for (String beanName : ctx.getBeanDefinitionNames()) {
 				if (beanName.contains("controller")) {
 					System.out.println(beanName);
