2faf52a0837736607072acae58415c6cad199cdb
hjkim1137 <withcusco@gmail.com>
2025-09-03 22:22:55 +0900

    수정: :zap: UserController, Dto valid 애너테이션 추가

--- src/main/java/com/diaconn_mall/website/controller/UserController.java
+++ src/main/java/com/diaconn_mall/website/controller/UserController.java
@@ -3,6 +3,7 @@ package com.diaconn_mall.website.controller;
 import com.diaconn_mall.website.dto.UserDto;
 import com.diaconn_mall.website.service.UserService;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,7 +29,7 @@ public class UserController {
     }
 
     @PostMapping(value = "/register", produces = "application/json")
-    public ResponseEntity<Map<String, String>> registerUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<Map<String, String>> registerUser(@Valid @RequestBody UserDto userDto) {
         try {
             System.out.println("@@@@회원가입 요청 도착: " + userDto);
             userService.registerUser(userDto);
@@ -40,7 +41,7 @@ public class UserController {
     }
 
     @PutMapping(value = "/update", produces = "application/json")
-    public ResponseEntity<Map<String, String>> updateUser(@RequestBody UserDto userDto, HttpSession session) {
+    public ResponseEntity<Map<String, String>> updateUser(@Valid @RequestBody UserDto userDto, HttpSession session) {
         try {
             System.out.println("@@@@마이페이지 수정 요청 도착: " + userDto);
             userService.updateUser(userDto, session); // 세션도 함께 업데이트
--- src/main/java/com/diaconn_mall/website/dto/UserDto.java
+++ src/main/java/com/diaconn_mall/website/dto/UserDto.java
@@ -1,5 +1,8 @@
 package com.diaconn_mall.website.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDto {
     private long id;
+    @NotBlank
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
     private String phone;
+    @NotBlank
+    @Size(max = 64, message = "비밀번호는 최대 64자까지 가능합니다.")
     private String password;
     private String address;
     private String addressDetail;
