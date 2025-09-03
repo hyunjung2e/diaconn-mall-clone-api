6d4c01df295e31d2320ca0f5999a637da8643f05
hjkim1137 <withcusco@gmail.com>
2025-09-03 21:58:48 +0900

    수정: :zap: OrderDto Valid 애너테이션 추가

--- src/main/java/com/diaconn_mall/website/dto/OrderDto.java
+++ src/main/java/com/diaconn_mall/website/dto/OrderDto.java
@@ -1,4 +1,9 @@
 package com.diaconn_mall.website.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.joda.time.LocalDateTime;
@@ -7,16 +12,23 @@ import java.util.List;
 
 @Getter
 @Setter
-
 public class OrderDto {
     private Long id;
+
+    @NotNull
+    @Positive
     private Long totalPrice;
+
     private LocalDateTime regDate;
     private String address;
     private String addressDetail;
     private String phone;
     private String name;
+
     private Long userId;
     private String memo;
+
+    @NotEmpty
+    @Valid
     private List<OrderDetailDto> orderDetails;
 }
